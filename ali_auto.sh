1e39146786c73a8fd5bc67e05bfd557a14255285
qingdog <qingdoor@gmail.com>
2024-09-30 09:29:15 +0800

    workflows

--- ali_auto.sh
+++ ali_auto.sh
@@ -3,7 +3,7 @@
 # author:kratos
 
 #你自己的refresh_token
-refresh_token="你自己的refresh_token"
+refresh_token="${REFRESH_TOKEN}"
 
 function get_json_value()
 {
@@ -24,8 +24,9 @@ function get_json_value()
 
 token=$(curl -s  -X POST -H "Content-Type: application/json" -d '{"grant_type": "refresh_token", "refresh_token": "'"$refresh_token"'"}' https://auth.aliyundrive.com/v2/account/token)
 
-
-access_token=$(get_json_value $token "access_token")
+#access_token=$(get_json_value $token "access_token")
+access_token=$(echo $token | grep -ioP '(?<="access_token":").*?(?=")')
+#echo "access_token 登录令牌：$access_token"
 
 # show token
 #echo $access_toekn
@@ -36,4 +37,20 @@ sign=$(curl -s -X POST -H "Content-Type: application/json" -H 'Authorization:Bea
 
 #不做验证
 #result=$(get_json_value $sign "success")
-echo "签到成功"
+#echo $result
+
+# 使用 grep 和 sed 提取 blessing 值
+success=$(echo $sign | grep -ioP '"success":.*?[,}]')
+title=$(echo $sign | grep -ioP '"title":.*?[,}]')
+subject=$(echo $sign | grep -ioP '"subject":.*?[,}]')
+signInCover=$(echo "$sign" | grep -ioP '"signInCover":.*?[,}]')
+# 输出 $title $subject $signInCover 值
+echo "$title $subject $signInCover"
+
+if [[ -z "$title" || "$success" != '"success":true,' ]]; then
+  code=$(echo "$sign" | grep -o '"code": *"[^"]*"' | sed 's/"code": *"\([^"]*\)"/\1/')
+  message=$(echo "$sign" | grep -o '"message": *"[^"]*"' )
+  echo "错误！！！：code: $code, $message"
+else
+  echo "签到执行状态： $success"
+fi
